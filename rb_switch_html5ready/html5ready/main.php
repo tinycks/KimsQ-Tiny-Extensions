9fb986f53e3c2cbfb5df520d141defe905ed477b
unknown <tiny@.(none)>
2013-07-30 12:51:28 +0900

    version 1.0.0

---
+++ rb_switch_html5ready/html5ready/main.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Tiny(타이니)
+ */
+?>
+
+<link rel="stylesheet" href="<?php echo $g['s'];?>/switchs/head/html5ready/normalize.min.css">
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<script>window.html5 || document.write('<script src="<?php echo $g['s'];?>/switchs/head/html5ready/html5shiv.js"><\/script>')</script>
+<script src="//ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+<script src="//ie7-js.googlecode.com/svn/version/2.1(beta4)/ie7-squish.js"></script>
+<![endif]-->
