f8c62a6bcacd5639c1e5b1e6e7d7da1184e90641
unknown <tiny@.(none)>
2013-07-30 12:51:45 +0900

    version 2.1.0

---
+++ rb_switch_smartcdn2/smartCdn2/main.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Tiny(타이니)
+ */
+$g['lib_jquery']=true;
+?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo $g['s'];?>/switchs/head/smartCdn2/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="<?php echo $g['s'];?>/switchs/head/smartCdn2/jquery-migrate-1.2.1.min.js"></script>
